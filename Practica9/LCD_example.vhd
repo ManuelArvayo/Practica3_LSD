d498ddcd252ec8e9a21152c9231279694aa0b998
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2021-04-24 12:02:16 -0500

    Add files via upload

---
+++ Practica9/LCD_example.vhd
@@ -0,0 +1,56 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY lcd_example IS
+  PORT(
+      clk       : IN  STD_LOGIC;  --system clock
+      rw, rs, e : OUT STD_LOGIC;  --read/write, setup/data, and enable for lcd
+      lcd_data  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --data signals for lcd
+END lcd_example;
+
+ARCHITECTURE behavior OF lcd_example IS
+  SIGNAL   lcd_enable : STD_LOGIC;
+  SIGNAL   lcd_bus    : STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL   lcd_busy   : STD_LOGIC;
+  COMPONENT lcd_controller IS
+    PORT(
+       clk        : IN  STD_LOGIC; --system clock
+       reset_n    : IN  STD_LOGIC; --active low reinitializes lcd
+       lcd_enable : IN  STD_LOGIC; --latches data into lcd controller
+       lcd_bus    : IN  STD_LOGIC_VECTOR(9 DOWNTO 0); --data and control signals
+       busy       : OUT STD_LOGIC; --lcd controller busy/idle feedback
+       rw, rs, e  : OUT STD_LOGIC; --read/write, setup/data, and enable for lcd
+       lcd_data   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --data signals for lcd
+  END COMPONENT;
+BEGIN
+
+  --instantiate the lcd controller
+  dut: lcd_controller
+    PORT MAP(clk => clk, reset_n => '1', lcd_enable => lcd_enable, lcd_bus => lcd_bus, 
+             busy => lcd_busy, rw => rw, rs => rs, e => e, lcd_data => lcd_data);
+  
+  PROCESS(clk)
+    VARIABLE char  :  INTEGER RANGE 0 TO 7 := 0;
+  BEGIN
+    IF(clk'EVENT AND clk = '1') THEN
+      IF(lcd_busy = '0' AND lcd_enable = '0') THEN
+        lcd_enable <= '1';
+        IF(char < 10) THEN
+          char := char + 1;
+        END IF;
+        CASE char IS
+          WHEN 1 => lcd_bus <= "1001001101"; -- M
+          WHEN 2 => lcd_bus <= "1001100001"; -- a
+          WHEN 3 => lcd_bus <= "1001101110"; -- n
+          WHEN 4 => lcd_bus <= "1001110101"; -- u
+          WHEN 5 => lcd_bus <= "1001100101"; -- e
+          WHEN 6 => lcd_bus <= "1001101100"; -- l
+          WHEN OTHERS => lcd_enable <= '0';
+        END CASE;
+      ELSE
+        lcd_enable <= '0';
+      END IF;
+    END IF;
+  END PROCESS;
+  
+END behavior;
