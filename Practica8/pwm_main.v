ef372c2fb3cb1bf13d0966d36065e363588477bb
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2021-04-25 16:59:52 -0500

    Add files via upload

---
+++ Practica8/pwm_main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/17/2021 10:04:35 PM
+// Design Name: 
+// Module Name: pwm_main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_main(
+
+    );
+endmodule
