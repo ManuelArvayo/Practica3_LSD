91b998a6cbe494086e7a4ccc5ea78080410b3517
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2021-05-24 14:04:25 -0500

    Update intensidad_focos.vhd

--- PROYECTO_FINAL/intensidad_focos.vhd
+++ PROYECTO_FINAL/intensidad_focos.vhd
@@ -14,7 +14,7 @@ architecture behavioral of dimmer is
   
   
 type stateType is (S0, S25, S50, S75, S100);
-	signal currentState, nextState : stateType;
+	signal currentState: stateType;
 
 subtype u20 is unsigned(19 downto 0);
 signal counter      : u20 := x"00000";
@@ -22,21 +22,27 @@ signal counter      : u20 := x"00000";
 constant clk_freq   : integer := 100_000_000;       -- Clock frequency in Hz (10 ns)
 constant pwm_freq   : integer := 50;                -- PWM signal frequency in Hz (20 ms)
 constant period     : integer := clk_freq/pwm_freq; -- Clock cycle count per PWM period
-signal duty_cycle : integer := 50000;            -- Clock cycle count per PWM duty cycle
+signal duty_cycle : integer := 0;            -- Clock cycle count per PWM duty cycle
 
 signal pwm_counter  : std_logic := '0';
 signal stateHigh    : std_logic := '1';
 
 begin
 
+currentState <=	S0  when state="000" else
+		S25 when state="001" else
+		S50 when state="010" else
+		S75 when state="011" else
+		S100;
+				
+	
   
 syncProcess: process(btn_in, Clk100m)
 variable cur : u20 := counter;
 	begin
-		if (rising_edge(Clk100m)) then
-			currentState <= nextState;
+	    if (rising_edge(Clk100m)) then
 			cur := cur + 1;  
-            counter <= cur;
+            		counter <= cur;
             if (cur <= duty_cycle) then
                  pwm_counter <= '1'; 
             elsif (cur > duty_cycle) then
@@ -52,61 +58,12 @@ variable cur : u20 := counter;
 
 	begin
 	case currentState is
-		when S0 =>
-    
-    
-			duty_cycle <= 50000;
-			if (btn_in = '1') then
-				nextState <= S45;
-			else
-				nextState <= S0;
-			end if;
-        
-        
-		when S25 =>
-        
-        
-		duty_cycle <= 100000;
-			if (btn_in = '1') then
-				nextState <= S90;
-			else
-				nextState <= S45;
-			end if;
-        
-        
-		when S50 =>
-        
-		duty_cycle <= 150000;
-			if (btn_in = '1') then
-				nextState <= S135;
-			else
-				nextState <= S90;
-			end if;
-        
-        
-		when S75 =>
-        
-        
-		duty_cycle <= 200000;
-			if (btn_in = '1') then
-				nextState <= S180;
-			else
-				nextState <= S135;
-			end if;
-        
-        
-		when S100 =>
-        
-        
-		duty_cycle <= 250000;
-			if (btn_in = '1') then
-				nextState <= S0;
-	        else
-	            nextState <= S180;
-			end if;
-        
-		when others =>
-			nextState <= S0;
+		when S0 => duty_cycle <= 0;    -- 0%
+		when S25 => duty_cycle <= 500000;    -- 25%
+		when S50 => duty_cycle <= 1000000;    -- 50%
+		when S75 => duty_cycle <= 1500000;      -- 75%
+		when S100 => duty_cycle <= 2000000;     -- 100%
+		when others => null;
 			
 	end case;
 	
