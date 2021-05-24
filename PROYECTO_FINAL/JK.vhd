19c1ee9767eaf079c94a909df44d7b3b198ec144
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2021-05-24 15:25:52 -0500

    Create JK.vhd

---
+++ PROYECTO_FINAL/JK.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity JK is
+port(
+  J,K,CLK: in std_logic;
+  Q: out std_logic
+  );
+  
+architecture behavioral of JK is
+
+begin
+
+process(J,K,CLK)
+	begin
+		if rising_edge(CLK) then
+			if(J='1') then
+			   	Q='1';
+			elsif(K='1') then
+				Q='0';
+			else null;
+			end if;
+		else null;
+		end if;
+	end process;
+
+end behavioral;
