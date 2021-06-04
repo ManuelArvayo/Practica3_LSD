952076aa7fc15693bb4cf513d9e7a5fd99983226
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2021-06-04 05:15:53 -0500

    Update print.vhd

--- PROYECTO_FINAL/print.vhd
+++ PROYECTO_FINAL/print.vhd
@@ -1,1 +1,64 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
 
+ENTITY lcd_example IS
+  PORT(
+      clk       : IN  STD_LOGIC;  --system clock
+      rw, rs, e : OUT STD_LOGIC;  --read/write, setup/data, and enable for lcd
+      lcd_data  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --data signals for lcd
+END lcd_example;
+
+ARCHITECTURE behavior OF lcd_example IS
+  SIGNAL   lcd_enable : STD_LOGIC;
+  SIGNAL   lcd_bus    : STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL   lcd_busy   : STD_LOGIC;
+  COMPONENT lcd_controller IS
+    PORT(
+       clk        : IN  STD_LOGIC; --system clock
+       reset_n    : IN  STD_LOGIC; --active low reinitializes lcd
+       lcd_enable : IN  STD_LOGIC; --latches data into lcd controller
+       lcd_bus    : IN  STD_LOGIC_VECTOR(9 DOWNTO 0); --data and control signals
+       busy       : OUT STD_LOGIC; --lcd controller busy/idle feedback
+       rw, rs, e  : OUT STD_LOGIC; --read/write, setup/data, and enable for lcd
+       lcd_data   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --data signals for lcd
+  END COMPONENT;
+BEGIN
+
+  --instantiate the lcd controller
+  dut: lcd_controller
+    PORT MAP(clk => clk, reset_n => '1', lcd_enable => lcd_enable, lcd_bus => lcd_bus, 
+             busy => lcd_busy, rw => rw, rs => rs, e => e, lcd_data => lcd_data);
+  
+  PROCESS(clk)
+    VARIABLE char  :  INTEGER:= 0;
+  BEGIN
+    IF(clk'EVENT AND clk = '1') THEN
+      IF(lcd_busy = '0' AND lcd_enable = '0') THEN
+        lcd_enable <= '1';
+        IF(char < 15) THEN
+          char := char + 1;
+        END IF;
+        CASE char IS
+          WHEN 1 => lcd_bus <= "1001001101"; -- M
+          WHEN 2 => lcd_bus <= "1001100001"; -- a
+          WHEN 3 => lcd_bus <= "1001101110"; -- n
+          WHEN 4 => lcd_bus <= "1001110101"; -- u
+          WHEN 5 => lcd_bus <= "1001100101"; -- e
+          WHEN 6 => lcd_bus <= "1001101100"; -- l
+          WHEN 7 => lcd_bus <= "0011000000"; -- salto de linea
+          WHEN 8 => lcd_bus <= "1001001010";  -- J
+          WHEN 9 => lcd_bus <= "1001100101";  -- e
+          WHEN 10 => lcd_bus<= "1001110011";  -- s
+          WHEN 11 => lcd_bus <= "1001110011"; -- s
+          WHEN 12 => lcd_bus <= "1001101001"; -- i
+          WHEN 13 => lcd_bus <= "1001100011"; -- c
+          WHEN 14 => lcd_bus <= "1001100001";  -- a
+          WHEN OTHERS => lcd_enable <= '0';
+        END CASE;
+      ELSE
+        lcd_enable <= '0';
+      END IF;
+    END IF;
+  END PROCESS;
+  
+END behavior;
